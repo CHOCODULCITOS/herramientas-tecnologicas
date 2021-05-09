8b71e56a30b01a318bf36c812d74ec82510646e3
CHOCODULCITOS <82556657+CHOCODULCITOS@users.noreply.github.com>
2021-05-08 22:38:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+       document.querySelector('.nav-menu').classList.toggle('activo');
+   });
+   document.querySelector('.menu-btn').addEventListener('click',()=>{
+       document.querySelector('.contenedor-header').classList.toggle('activo');
+   });
+
+   AOS.init();
+
+
